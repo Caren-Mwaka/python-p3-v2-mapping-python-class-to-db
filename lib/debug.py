ef08621867a13e814eff6b4b44bc7c193349bf63
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-06-08 11:29:06 +0300

    modified department.py

--- lib/debug.py
+++ lib/debug.py
@@ -4,29 +4,17 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
-#!/usr/bin/env python3
-
-from __init__ import CONN, CURSOR
-from department import Department
-
 import ipdb
 
-Department.drop_table()
-Department.create_table()
 
-payroll = Department.create("Payroll", "Building A, 5th Floor")
-print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+def reset_database():
+    Department.drop_table()
+    Department.create_table()
 
-hr = Department.create("Human Resources", "Building C, East Wing")
-print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+    Department.create("Payroll", "Building A, 5th Floor")
+    Department.create("Human Resources", "Building C, East Wing")
+    Department.create("Accounting", "Building B, 1st Floor")
 
-hr.name = 'HR'
-hr.location = "Building F, 10th Floor"
-hr.update()
-print(hr)  # <Department 2: HR, Building F, 10th Floor>
 
-print("Delete Payroll")
-payroll.delete()  # delete from db table, object still exists in memory
-print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
-
+reset_database()
 ipdb.set_trace()
